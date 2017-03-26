63cb5c8dc05abaaefe59429515cb5b419faa9402
Avidor <avidor98@gmail.com>
2017-03-26 09:29:57 +0300

    broken method for msg all clients

--- server.py
+++ server.py
@@ -1,33 +1,56 @@
 import SocketServer
 import thread
+import sys
+
+
 queue = ''
 
+clients = []
 
-class CommandHandler:
+
+def add_client(conn, name):
+    clients.append({'conn': conn, 'name': name, 'queue': []})
+
+
+def msg_all_clients(message):
+    for client in clients:
+        print client.queue.append(message)
+        globals()[client['conn']].request.sendall(message)
+
+
+class CommandHandler():
     def test(self, args):
         print("actual test")
         print args
 
+    def msg(self, args):
+        print "sending: " + args
+        msg_all_clients(args)
+
 
 class ConnectionHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-        global queue
+        global queue, clients
 
         self.data = self.request.recv(1024).strip()
         print "incoming connection from {}".format(self.client_address[0])
 
+        add_client(self, self.client_address[0])
+        print clients
+
+
+
         # todo flow: validate -> reject and close connection if bad -> listen for orders
 
-        while True:
-            if queue:
-                self.request.sendall(queue)
-                if queue == 'close':
-                    break
-                queue = ''
+        # while True:
+        #     if queue:
+        #         self.request.sendall(queue)
+        #         if queue == 'close':
+        #             break
+        #         queue = ''
 
 
 def main():
-    global queue
     HOST, PORT = "localhost", 9999  # todo read config
     server = SocketServer.TCPServer((HOST, PORT), ConnectionHandler)
     thread.start_new_thread(server.serve_forever, ())
