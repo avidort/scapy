60184e31f54d50ad943e90ae2820820b3f717b4c
Avidor <avidor98@gmail.com>
2017-03-27 08:51:24 +0300

    Fix message queue for constant client-server communication

--- server.py
+++ server.py
@@ -1,21 +1,18 @@
 import SocketServer
 import thread
-import sys
-
 
 queue = ''
-
 clients = []
 
 
-def add_client(conn, name):
-    clients.append({'conn': conn, 'name': name, 'queue': []})
+def add_client(name, ipaddr):
+    clients.append({'name': name, 'ipaddr': ipaddr, 'queue': []})
+    return len(clients) - 1
 
 
 def msg_all_clients(message):
     for client in clients:
-        print client.queue.append(message)
-        globals()[client['conn']].request.sendall(message)
+        client['queue'].append(message)
 
 
 class CommandHandler():
@@ -24,8 +21,8 @@ class CommandHandler():
         print args
 
     def msg(self, args):
-        print "sending: " + args
-        msg_all_clients(args)
+        print "sending: " + args[0]
+        msg_all_clients(args[0])
 
 
 class ConnectionHandler(SocketServer.BaseRequestHandler):
@@ -35,19 +32,18 @@ class ConnectionHandler(SocketServer.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print "incoming connection from {}".format(self.client_address[0])
 
-        add_client(self, self.client_address[0])
-        print clients
-
-
+        client = add_client('my client', self.client_address[0])
 
         # todo flow: validate -> reject and close connection if bad -> listen for orders
 
-        # while True:
-        #     if queue:
-        #         self.request.sendall(queue)
-        #         if queue == 'close':
-        #             break
-        #         queue = ''
+        while True:
+            for task in clients[client]['queue']:
+                if task:
+                    print task
+                    self.request.sendall(task)
+                    if task == 'close':
+                        break
+                    clients[client]['queue'].remove(task)
 
 
 def main():
@@ -64,7 +60,7 @@ def main():
             args = []
 
         try:
-            eval('CommandHandler().{0}("{1}")'.format(cmd, args))
+            eval('CommandHandler().{0}({1})'.format(cmd, args))
         except AttributeError:
             print "Unknown command: {0}".format(cmd)
 
